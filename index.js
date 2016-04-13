541d4a228d38c6bca4d809be1136fe726e868c43
Yash Shah <mail@yashshah.com>
2016-04-13 18:29:39 +0530

    Use Appbase webhooks

--- index.js
+++ index.js
@@ -44,8 +44,9 @@ app.get('/get_product_details', function (req, res) {
 app.post('/set_alert', function (req, res) {
   /* Starting polling for the requested product */
   helper.index_product(req.param('product_id'));
+  mail_html_content = "<p>You have set the price alert for flipkart product <b>"+req.param('product_id')+"</b>. Your condition has been matched and Price has reached to <b>{{{price}}}</b></p>";
   /* Starting stream search for the user condition */
-  appbase.searchStream({
+  appbase.searchStreamToURL({
       type: 'flipkart_app',
       body: {
         "query": {
@@ -64,20 +65,16 @@ app.post('/set_alert', function (req, res) {
           }
         }
       }
+  }, {
+    'method': 'POST',
+    'url': 'https://api.sendgrid.com/api/mail.send.json',
+    'headers': {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    "count":1,
+    'body': 'to='+req.param('email')+'&amp;toname=Yash&amp;subject=Flipkart Price Alert&amp;html='+mail_html_content+'&amp;text=Price reached to '+req.param('lte')+'&amp;from=Appbase.io&amp;api_user=yashshah&amp;api_key=appbase12'
   }).on('data', function(response) {
-      console.log("new document update: ", response);
-      html = "<p>You have set the price alert for flipkart product <b>"+req.param('product_id')+"</b>. Your condition has been matched and Price has reached to <b>"+response._source.price+"</b></p>";
-      var options = {
-      'method': 'POST',
-      'uri': 'https:\\\\api.sendgrid.com\\api\\mail.send.json',
-      'headers': {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      'body': 'to='+req.param('email')+'&amp;toname=Yash&amp;subject=Flipkart Price Alert&amp;html='+html+'&amp;text=Price reached to '+req.param('lte')+'&amp;from=Appbase.io&amp;api_user=yashshah&amp;api_key=appbase12'
-      };
-      request(options, function (error, response, body) {if(error){console.log(error);}});
-      console.log('mail sent to :- '+req.param('email'));
-      this.stop();
+      console.log("Webhook has been configured : ", response);
   }).on('error', function(error) {
       console.log("getStream() failed with: ", error)
   })
